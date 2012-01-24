f1d56fddf988b4beb0709670251f593f1e71eb73
Ingo Fründ <ingo.fruend@googlemail.com>
2012-01-24 12:29:26 +0100

    [py/NF] Doing bayes factors analytically

--- pypsignifit/bayes_factor.py
+++ pypsignifit/bayes_factor.py
@@ -9,7 +9,9 @@ __doc__ = """This module is intended to perform bayesian model comparison using 
 The rationale is the following:
 Imaginge a number of candidate psychometric function models M1,M2,...Mn. We proceed by treating
 M as another model parameter and perform gibbs sampling in the parameter space of this metamodel.
-The function gibbs model will then give the marginal posterior distribution in model space.
+The function gibbs_model will then give the marginal posterior distribution in model space using gibbs sampling.
+Interestingly, for this particular gibbs sampler, we can write out an analytical expression for the marginal stationary
+distribution with respect to M. The function gibbs_analytical determines this marginal distribution.
 
 In some applications, we might be interested in fitting more than one psychometric function in each
 of the models to be compared. This is for example relevant if the feature that distinguishes between
@@ -20,7 +22,7 @@ Note that the logic of this module only works for Bayesian inference and *not* f
 Also be aware that the data that were used for the different models should be the same across M1,M2,...,Mn.
 """
 
-__all__ = ["gibbs_model"]
+__all__ = ["gibbs_model","gibbs_analytical"]
 
 def sample_model ( theta, inference_objects ):
     """sample_model ( theta, inference_objects )
@@ -97,3 +99,52 @@ def gibbs_model ( inference_objects, nsamples=1000, M0=0 ):
         chain[i] = M
 
     return mpost,chain
+
+def gibbs_analytical ( inference_objects ):
+    """gibbs_analytical ( inference_objects )
+
+    Analytically determine the marginal stationary distribution of M in the
+    full model P(M,theta).
+
+    :Parameters:
+        *inference_objects* :
+            psychometric function models to be compared. Note that these inference objects
+            have to be based on the same data set in order to obtain valid results.
+    """
+
+    n = len ( inference_objects )
+    T = np.zeros ( (n,n), 'd' )
+
+    # Set up the transition matrix
+    for k,M in enumerate ( inference_objects ):
+        if isinstance ( M, psignidata.PsiInference ):
+            M = [M]
+        elif getattr ( M, "__iter__", False ):
+            pass
+        else:
+            raise ValueError, "Elements of inference_objects should be either a PsiInference object or a sequence"
+
+        for j in xrange ( M[0].mcestimates.shape[0] ):
+            th = [ m.mcestimates[j] for m in M ]
+            p =  np.exp ( np.array ( [ eval_post ( th, M_l ) for M_l in inference_objects ] ) )
+            p /= p.sum()
+            T[k] += p
+        T[k] /= T[k].sum()
+
+    # Now solve the stochastic matrix equation pi*T=pi
+    A = (T-np.eye(n)).T
+    A[0,:] = 1.
+    p = np.zeros ( n, 'd' )
+    p[0] = 1.
+
+    return np.linalg.solve ( A, p ),T
+
+def eval_post ( theta, M ):
+    if isinstance ( M, psignidata.PsiInference ):
+        M = [M]
+    elif getattr ( M, "__iter__", False ):
+        pass
+    else:
+        raise ValueError, "Elements of inference_objects should be either a PsiInference object or a sequence"
+
+    return reduce ( lambda x,y: x+y, [ -m._pmf.neglpost ( th, m._data ) for th,m in zip(theta,M) ] )
